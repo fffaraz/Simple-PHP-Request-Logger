c83ca17d4323af8e861856bc6ff75b22ef897601
Faraz Fallahi <fffaraz@gmail.com>
2013-11-01 19:34:57 +0330

    table

--- index.php
+++ index.php
@@ -8,10 +8,51 @@
 
 <!-- Latest compiled and minified JavaScript -->
 <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.1/js/bootstrap.min.js"></script>
+
+<!-- DataTables CSS -->
+<link rel="stylesheet" type="text/css" href="http://ajax.aspnetcdn.com/ajax/jquery.dataTables/1.9.4/css/jquery.dataTables.css">
+ 
+<!-- jQuery -->
+<script type="text/javascript" charset="utf8" src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.8.2.min.js"></script>
+ 
+<!-- DataTables -->
+<script type="text/javascript" charset="utf8" src="http://ajax.aspnetcdn.com/ajax/jquery.dataTables/1.9.4/jquery.dataTables.min.js"></script>
+
+<script type="text/javascript">
+
+$(document).ready(function() {
+    $('#example').dataTable( {
+        "sScrollY": "200px",
+        "bPaginate": false
+    } );
+} );
+ 
+function fnShowHide( iCol )
+{
+    /* Get the DataTables object again - this is not a recreation, just a get of the object */
+    var oTable = $('#example').dataTable();
+     
+    var bVis = oTable.fnSettings().aoColumns[iCol].bVisible;
+    oTable.fnSetColumnVis( iCol, bVis ? false : true );
+}
+
+</script>
+
 </head>
 <body>
+<a href="javascript:void(0);" onclick="fnShowHide(0);">id<br></a>
+<a href="javascript:void(0);" onclick="fnShowHide(1);">datetime<br></a>
+<a href="javascript:void(0);" onclick="fnShowHide(2);">ip<br></a>
+<a href="javascript:void(0);" onclick="fnShowHide(3);">hostname<br></a>
+<a href="javascript:void(0);" onclick="fnShowHide(4);">uri<br></a>
+<a href="javascript:void(0);" onclick="fnShowHide(5);">agent<br></a>
+<a href="javascript:void(0);" onclick="fnShowHide(6);">referer<br></a>
+<a href="javascript:void(0);" onclick="fnShowHide(7);">domain<br></a>
+<a href="javascript:void(0);" onclick="fnShowHide(8);">filename<br></a>
+<a href="javascript:void(0);" onclick="fnShowHide(9);">method<br></a>
+<a href="javascript:void(0);" onclick="fnShowHide(9);">data<br></a>
 <center>
-<table class="table table-striped table-bordered table-condensed" >
+<table class="table table-striped table-bordered table-condensed table-hover" id="example">
 <tr>
 <th>id</th>
 <th>datetime</th>
